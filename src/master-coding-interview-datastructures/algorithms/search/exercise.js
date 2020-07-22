7a969afffb4292590aeb13fbdfa6441ab09626b1
integreator <walterfrontier@gmail.com>
2020-07-22 03:45:55 -0400

    The course search algorithms section complete

---
+++ src/master-coding-interview-datastructures/algorithms/search/exercise.js
@@ -0,0 +1,17 @@
+/**
+ * The course first exercise;
+ * breadth firt search(BFS) or deepth firset search(DFS);
+ */
+
+//If you know a solution is not far from the root of the tree:
+BFS;
+//If the tree is very deep and solutions are rare:
+BFS;
+//If the tree is very wide:
+DFS;
+//If solutions are frequent but located deep in the tree:
+DFS;
+//Determining whether a path exists between two nodes:
+DFS;
+//Finding the shortest path:
+DFS; // This is worry! it should be BFS
